97f54724c987d904ee3d56e4429f85714b5d8d6f
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-05-25 22:55:25 +0800

    测试笔记备份的注释->笔记备份2012/05/25_22:55:25

--- conffile_backup/bin/noteupdate.sh
+++ conffile_backup/bin/noteupdate.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 #update loacl date(conf back and note) to remote sever
 cd ~/note
-~/Mysoft/bin/gitupdate.sh '文件备份'
+#date +%Y/%m/%d_%H:%M:%S
+if [ "$1" == "" ]
+then
+	~/Mysoft/bin/gitupdate.sh '笔记备份'`date +%Y/%m/%d_%H:%M:%S`
+else
+	~/Mysoft/bin/gitupdate.sh $1'->笔记备份'`date +%Y/%m/%d_%H:%M:%S`
+fi
