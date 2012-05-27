3581809aca01c0c31264a971d7bee1792cef07da
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-05-27 15:38:24 +0800

    笔记备份2012/05/27_15:38:24

--- conffile_backup/bin/confback.sh
+++ conffile_backup/bin/confback.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #backup some backup file (like vim bash) to loacl floder
-cp ~/.vimrc ~/note/conffile_backup
-cp ~/.gvimrc ~/note/conffile_backup
-cp ~/.bashrc ~/note/conffile_backup
-cp ~/.bash_profile ~/note/conffile_backup
+cfgpath=~/note/conffile_backup
+cp ~/.vimrc 		~/note/conffile_backup
+cp ~/.gvimrc 		~/note/conffile_backup
+cp ~/.bashrc		~/note/conffile_backup
+cp ~/.bash_profile 	~/note/conffile_backup
+cp /etc/hosts		$cfgpath
 cp ~/Mysoft/bin ~/note/conffile_backup/ -R
 #cd ~/note
 #~/Mysoft/bin/gitupdate.sh '文件备份'
