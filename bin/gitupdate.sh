83f19859b79dbf4c6081b1f89162e3dd130ac7a6
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-08-28 15:00:58 +0800

    笔记备份2012/08/28_15:00:58

--- bin/gitupdate.sh
+++ bin/gitupdate.sh
@@ -2,11 +2,11 @@
 #设置回显执行的语句 + 号
 set -x 
 #添加文件新增至工程
-git add . 
+#git add . 
 if [ "$1" == "" ]
 then
- 	git commit -a '没有说明' #添加说明?
+	git commit -a -m '没有说明' #添加说明? m提交 a默认跟踪
 else
- 	git commit -a $1 #添加说明?
+ 	git commit -a -m $1 #添加说明?
 fi
  git push -u origin master  #上传至服务器
