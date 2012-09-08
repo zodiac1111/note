245a9bb066c76d8a511ad34605001a8e06a29f84
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-09-08 09:28:33 +0800

    修改说明

--- bin/noteupdate.sh
+++ bin/noteupdate.sh
@@ -7,9 +7,9 @@ cd ~/note
 if [ "$1" == "" ]
 then
 	echo "默认说明"
-	~/Mysoft/bin/gitnoteupdate.sh '笔记备份'`date +%Y/%m/%d_%H:%M:%S`
+	~/Mysoft/bin/gitnoteupdate.sh '这次笔记修改没有特殊的说明:P'
 else
 	echo "手动说明"
-	~/Mysoft/bin/gitnoteupdate.sh $1'->笔记备份'`date +%Y/%m/%d_%H:%M:%S`
+	~/Mysoft/bin/gitnoteupdate.sh $1
 fi
 exit 0
