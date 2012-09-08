c20e247a8f442071685aece90e1aa7d0fe0de4a1
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-09-08 09:26:15 +0800

    on

--- bin/noteupdate.sh
+++ bin/noteupdate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #set -x
-set -v on
+#set -v on
 #update loacl date(conf back and note) to remote sever
 cd ~/note
 #date +%Y/%m/%d_%H:%M:%S
 if [ "$1" == "" ]
 then
+	echo "默认说明"
 	~/Mysoft/bin/gitnoteupdate.sh '笔记备份'`date +%Y/%m/%d_%H:%M:%S`
 else
+	echo "手动说明"
 	~/Mysoft/bin/gitnoteupdate.sh $1'->笔记备份'`date +%Y/%m/%d_%H:%M:%S`
 fi
 exit 0
