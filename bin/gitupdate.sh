ba92f26a8b1737747949496256d16daaea5ba098
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-09-08 09:08:40 +0800

    update

--- bin/gitupdate.sh
+++ bin/gitupdate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #设置回显执行的语句 + 号
-set -x 
+#set -x 
 #添加文件新增至工程
 #git add . 
 if [ "$1" == "" ]
 then
 	#-a 只能跟踪已经被跟踪的内容,不能跟踪新增的文件
 	#git commit -a -m '没有说明' #添加说明? m提交 a默认跟踪
-	echo "为了可追溯,清填写commit说明"
+	echo "为了可追溯,请填写commit说明"
 	echo "<gitupdate> <修改的某某某>"
 else
  	git commit -a -m $1 #添加说明?
