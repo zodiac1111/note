753ec3c2983a57e9cd06d2a94ce7af9fedd9eb30
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-03-11 21:59:16 +0800

    xx

--- bin/get_vps_file.sh
+++ bin/get_vps_file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # rsync -v -P -e ssh = rscp (alias)
-rsync -v -P -e ssh -avH root@vps:/var/www/html/bt/ ~/Downloads/bt/
+rsync -v -P -e ssh -avH root@vps:/var/www/html/bt/ ~/Downloads/.bt/
