d00b46d6ab7f13c69321e66a139e5272a1e5ef89
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-13 17:47:39 +0800

    v6?

--- bin/get_vps_file.sh
+++ bin/get_vps_file.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # rsync -v -P -e ssh = rscp (alias)
-rsync -v -P -e ssh -avH root@vps:/var/www/html/bt/ ~/Downloads/.bt/
+rsync -v -P -e ssh -avH root@vps:/var/www/html/bt/ ~/Downloads/.bt/ --include="*.avi" --include="*.mp4" --include="*.jpg" --include="*.rmvb" --include="*.MP4" --include="*.wmv" --exclude="*.*"
+
