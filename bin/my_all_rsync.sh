a67fae30620c10aa7ee116b00e724bf6f4226265
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-05-28 10:32:53 +0800

    note update

--- bin/my_all_rsync.sh
+++ bin/my_all_rsync.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # 同步所有需要同步的东西,很耗时间!! 一般在晚上没事干的时候同步
+# 同步目录
+backdir=/media/zodiac1111/bcb95e11-a242-494e-bd4c-b88f5fd3308d/
 echo "start rsync @" `date +"%Y-%m-%d %H:%M:%S"` >> /home/zodiac1111/rsync.log
-rsync '/home/zodiac1111/Music/' '/media/usb0/Music/'  -avH
-rsync '/home/zodiac1111/tmp/' '/media/usb0/tmp/'  -avH
-rsync '/home/zodiac1111/Mysoft/' '/media/usb0/Mysoft/' -avH
-rsync '/home/zodiac1111/Downloads/' '/media/usb0/Downloads/' -avH
-rsync '/home/zodiac1111/Documents/' '/media/usb0/Documents/' -avH
-rsync '/home/zodiac1111/Dropbox/' '/media/usb0/Dropbox/' -avH
-rsync '/home/zodiac1111/workspace/' '/media/usb0/workspace/' -avH
+rsync ~/ ${backdir}  -avH
+#rsync '/home/zodiac1111/tmp/' ${backdir}/tmp/'  -avH
+#rsync '/home/zodiac1111/Mysoft/' ${backdir}/Mysoft/' -avH
+#rsync '/home/zodiac1111/Downloads/' '/media/usb0/Downloads/' -avH
+#rsync '/home/zodiac1111/Documents/' '/media/usb0/Documents/' -avH
+#rsync '/home/zodiac1111/Dropbox/' '/media/usb0/Dropbox/' -avH
+#rsync '/home/zodiac1111/workspace/' '/media/usb0/workspace/' -avH
 echo "end rsync @" $? `date +"%Y-%m-%d %H:%M:%S"` >> /home/zodiac1111/rsync.log
--- config/.bash_aliases
+++ config/.bash_aliases
@@ -1,5 +1,6 @@
 # alias TODO 按照debian的方式将alias放到特定文件中分开管理
 alias rscp='rsync -v -P -e ssh' # 支持断点续传的scp
+alias rscp2='rsync -v -P -e "ssh -p 2200"' # 支持断点续传的scp
 alias vps='ssh root@1.gbzdsq.com' #登录vps
 alias vh='vim /etc/hosts' # 快速打开hosts文件
 alias yum='apt-get' #习惯
