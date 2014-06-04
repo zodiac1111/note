e95ef2312a54b2741474c57eeee452e32874a5a0
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-06-04 20:20:23 +0800

    整理动态壁纸生成脚本

--- bin/mkwp.sh
+++ bin/mkwp.sh
@@ -1,5 +1,7 @@
-#壁纸文件生成器
-#mkwp.sh <图片1> <图片2> ... <图片n> >  background-1.xml
+# 壁纸文件生成器,默认就是生成 background-1.xml 文件 
+# cd <想要生成的目录>
+# mkwp.sh <图片1> <图片2> ... <图片n> >  [background-1.xml]
+# 图片应该是绝对路径.可以直接从文件中拖拽到命令行
 
 ###########   可供设置的参数   ##########
 
@@ -16,16 +18,16 @@ filename="background-1.xml"
 #############  生成文件部分  #############
 
 # 生成文件头
-echo -e "<background>" 				> ${filename}
-echo -e "\t<starttime>"				>> ${filename}
-echo -e "\t\t<year>2009</year>"		>> ${filename}
-echo -e "\t\t<month>08</month>">> ${filename}
-echo -e "\t\t<day>04</day>">> ${filename}
-echo -e "\t\t<hour>00</hour>">> ${filename}
-echo -e "\t\t<minute>00</minute>">> ${filename}
-echo -e "\t\t<second>00</second>">> ${filename}
-echo -e "\t</starttime>">> ${filename}
-echo -e "<!-- This animation will start at midnight. -->">> ${filename}
+echo -e "<background>" 										> ${filename}
+echo -e "\t<starttime>"										>> ${filename}
+echo -e "\t\t<year>2009</year>"								>> ${filename}
+echo -e "\t\t<month>08</month>"								>> ${filename}
+echo -e "\t\t<day>04</day>"									>> ${filename}
+echo -e "\t\t<hour>00</hour>"								>> ${filename}
+echo -e "\t\t<minute>00</minute>"							>> ${filename}
+echo -e "\t\t<second>00</second>"							>> ${filename}
+echo -e "\t</starttime>"									>> ${filename}
+echo -e "<!-- This animation will start at midnight. -->"	>> ${filename}
 
 # 遍历所有参数给出的图片文件
 for(( i=1; i <= $#; ++i ))
@@ -40,18 +42,18 @@ do
 			 next="$(eval echo "\$1")";
 		fi
 		# 构造xml文本(循环部分)
-		echo -e "\t<static>">> ${filename}
-		echo -e "\t\t<duration>"$stoptime"</duration>">> ${filename}
-		echo -e "\t\t<file>"${pre}"</file>">> ${filename}
-		echo -e "\t</static>">> ${filename}
-  		echo -e "\t<transition>">> ${filename}
-  		echo -e "\t\t<duration>"$changetime"</duration>">> ${filename}
-  		echo -e "\t\t<from>"${pre}"</from>">> ${filename}
-  		echo -e "\t\t<to>"$next"</to>">> ${filename}
-		echo -e "\t</transition>">> ${filename}
+		echo -e "\t<static>" 								>> ${filename}
+		echo -e "\t\t<duration>"$stoptime"</duration>"		>> ${filename}
+		echo -e "\t\t<file>"${pre}"</file>"					>> ${filename}
+		echo -e "\t</static>"								>> ${filename}
+  		echo -e "\t<transition>"							>> ${filename}
+  		echo -e "\t\t<duration>"$changetime"</duration>"	>> ${filename}
+  		echo -e "\t\t<from>"${pre}"</from>"					>> ${filename}
+  		echo -e "\t\t<to>"$next"</to>"						>> ${filename}
+		echo -e "\t</transition>"							>> ${filename}
 done
 
 # 文件尾
-echo -e "</background>">> ${filename}
+echo -e "</background>"										>> ${filename}
 
 echo -e "共 \e[32m"$#"\e[0m 个图像.生成完成: \e[32m"${filename}"\e[0m"
