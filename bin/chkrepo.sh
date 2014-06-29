b0941eb00206601cb8da84fe9481628b1bea688c
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-06-29 12:58:41 +0800

    说明更正

--- bin/chkrepo.sh
+++ bin/chkrepo.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # file chkreop.sh  检查所有仓库是否push
+
+# 反注释以调试脚本
 # set -x
+
 # 初始化,所有仓库路径 
 # ********  在这里指定所有仓库的路径  *********
 aRepos=(
@@ -18,18 +21,24 @@ aRepos=(
 )
 
 # **********************************************
-n=0 #未push的仓库数量
+n_commit=0 #未push的仓库数量
+n_add=0
+n_push=0
 lenRepo=${#aRepos[@]} 
 
 #函数 检查是否有未push的提交 
 # pro <dir> 转到dir目录下,查询git是否提交,提交返回0,否则返回1
+# 检查仓库状态,
+# 0 pushed
+# 1 commit 但是未push
+# 2 add 但是未commit
 function isPushed()
 {
 	#echo dir=$1
 	cd $1
 	# 发生错误,可能是没有这个仓库,退出
 	if [ $? != "0" ]; then
-		return 1	
+		return 99	
 	fi
 	# 判断返回的字符
 	git st|grep "您的分支领先" >>/dev/null
@@ -42,19 +51,19 @@ function isPushed()
 	# 返回提示
 	if [ $ret == "1" ] && [ $ret_m == "1" ] && [ $ret_a == "1" ]; then
 		echo -e "[\e[32m Pushed \e[0m] "`pwd`
-		return 1
+		return 0
 	elif [ $ret_m == "0" ]; then
 		echo -e "[\e[31m Commit \e[0m] \e[33m"`pwd`"\e[0m"
-		return 0
+		return 1
 	elif [ $ret == "0" ]; then
 		echo -en "[\e[33m Unpush \e[0m] "`pwd`
 		echo -en "\r[\e[32m Push.. \e[0m] "
 		git push > /dev/null 2>&1
 		echo -e "\r[\e[32m Pushed \e[0m] "
-		return 1
+		return 0
 	elif [ $ret_a == "0" ]; then
 		echo -e "[\e[31m NonAdd \e[0m] \e[33m"`pwd`"\e[0m"
-		return 0
+		return 2
 	fi
 }
 
@@ -65,18 +74,23 @@ while [ $i -lt $lenRepo ]
 do
 	#echo i=$i ${aRepos[$i]}
 	isPushed ${aRepos[$i]}
-	if [ $? = "0" ] ; then
-		let n++
+	ret=$?
+	if [ $ret == "0" ] ; then
+		let n_push++
+	elif [ $ret == "1" ]; then
+		let n_commit++
+	elif [ $ret == "2" ]; then
+		let n_add++
 	fi
 	let i++
 done
 
 #检查完毕,总结
 echo -en "\e[32m 检查完毕\e[0m: "
-if [ $n == "0" ]; then
+if [ $n_commit == "0" ] && [ $n_add == "0" ]; then
 	echo -en "所有仓库均已push"
 else
-	echo -en "有 \e[31m$n\e[0m 个仓库没有push"
+	echo -en "有 \e[31m$n_commit\e[0m 个仓库没有commit"
 fi
 echo ""
 
