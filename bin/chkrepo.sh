75e55e6f5922d4efdfde5bd0ce77d2727452f193
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-06-29 10:34:32 +0800

    输出的格式

--- bin/chkrepo.sh
+++ bin/chkrepo.sh
@@ -45,9 +45,9 @@ function isPushed()
 		return 0
 	elif [ $ret == "0" ]; then
 		echo -e "[\e[33m Unpush \e[0m] "`pwd`
-		echo -e "\t\e[32m Pushing... \e[0m"
+		echo -e "\t  \e[32m Pushing... \e[0m\r"
 		git push > /dev/null 2>&1
-		echo -e "\v[\e[32m   OK   \e[0m] Pushed:"`pwd`
+		echo -e "\v[\e[32m   OK   \e[0m] Pushed: "`pwd`
 		return 0
 	elif [ $ret_a == "0" ]; then
 		echo -e "[\e[31m NonAdd \e[0m] "`pwd`
