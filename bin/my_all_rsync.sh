40c4bce30404c5847c35ebf8645d3b794c707611
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-10-23 20:25:37 +0800

    备份方式

--- bin/my_all_rsync.sh
+++ bin/my_all_rsync.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # 同步所有需要同步的东西,很耗时间!! 一般在晚上没事干的时候同步
 # 同步目录
+
+# 缓存密码
+echo "开始备份home目录"
+
 backdir=/media/zodiac1111/bcb95e11-a242-494e-bd4c-b88f5fd3308d/
 echo "start rsync @" `date +"%Y-%m-%d %H:%M:%S"` >> /home/zodiac1111/rsync.log
 rsync --exclude .cache --exclude .local/share/Trash -avH ~/  ${backdir}  
@@ -11,3 +15,10 @@ rsync --exclude .cache --exclude .local/share/Trash -avH ~/  ${backdir}
 #rsync '/home/zodiac1111/Dropbox/' '/media/usb0/Dropbox/' -avH
 #rsync '/home/zodiac1111/workspace/' '/media/usb0/workspace/' -avH
 echo "end rsync @" $? `date +"%Y-%m-%d %H:%M:%S"` >> /home/zodiac1111/rsync.log
+
+echo "卸载备份硬盘"
+# 用完卸载
+umount /media/zodiac1111/bcb95e11-a242-494e-bd4c-b88f5fd3308d
+
+# 可能可以关机
+#sudo 
