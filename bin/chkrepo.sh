4617057c01f86bfe596374451a79ce29cb30342e
zodiac1111 <zodiac8765+github_admin@gmail.com>
2015-08-30 15:36:02 +0800

    升级仓库push程序,整理一下逻辑

--- bin/chkrepo.sh
+++ bin/chkrepo.sh
@@ -2,9 +2,10 @@
 # file chkreop.sh  检查所有仓库是否push
 
 # 反注释以调试脚本
-#set -x
+# set -x # 查看运行的命令
+# set -v # 查看bash输入的文本
 
-# 初始化,所有仓库路径 
+# 初始化,所有仓库路径
 # ********  在这里指定所有仓库的路径  *********
 aRepos=(
 "$HOME/dotvim"
@@ -23,25 +24,56 @@ aRepos=(
 "$HOME/Dropbox/hv插件/freeshop发送"
 "$HOME/tmp/webbox_manual"
 "$HOME/tmp/lisp"
+"$HOME/workspace/lua/c_call_lua_pass_data_to_lua"
 )
 
 # **********************************************
 n_commit=0 #未push的仓库数量
 n_add=0
 n_push=0
-lenRepo=${#aRepos[@]} 
+lenRepo=${#aRepos[@]}
 
 # 仓库状态
 rstat=0
 
 function main() {
-	echo "main function";
+	#echo de: $aRepos n= $lenRepo .
+	#检查所有仓库
+	i=0
+	while [ $i -lt $lenRepo ]
+	do
+		#echo i=$i ${aRepos[$i]}
+		isPushed ${aRepos[$i]};ret=$?
+		if [ $ret == "0" ] ; then
+			let n_push++
+		elif [ $ret == "1" ]; then
+			let n_commit++
+		elif [ $ret == "2" ]; then
+			let n_add++
+		fi
+		let i++
+	done
+
+	#检查完毕,总结
+	echo -en "[${CL_GREEN}检查完毕${CL_END}]"
+	if [ $n_commit == "0" ] && [ $n_add == "0" ]; then
+		echo -e "所有仓库均已push"
+	else
+		if [ $n_commit != "0" ]; then
+			echo -e "\n* 有 ${CL_RED}${n_commit}${CL_END} 个仓库没有commit"
+		fi
+		if [ $n_add != "0" ]; then
+			echo -e "\n* 有 ${CL_RED}${n_add}${CL_END} 个仓库没有add"
+		fi
+	fi
+	echo ""
 }
 
 function repo_stat() {
 	return
 }
-#函数 检查是否有未push的提交 
+
+#函数 检查是否有未push的提交
 # pro <dir> 转到dir目录下,查询git是否提交,提交返回0,否则返回1
 # 检查仓库状态,
 # 0 pushed
@@ -52,7 +84,7 @@ function isPushed() {
 	cd $1
 	# 发生错误,可能是没有这个仓库,退出
 	if [ $? != "0" ]; then
-		return 99	
+		return 99
 	fi
 	# 判断返回的字符
 	git st|grep "您的分支领先" >>/dev/null
@@ -64,54 +96,35 @@ function isPushed() {
 
 	# 返回提示
 	if [ $ret == "1" ] && [ $ret_m == "1" ] && [ $ret_a == "1" ]; then
-		echo -e "[\e[32m Pushed \e[0m] "`pwd`
+		echo -e "[${CL_GREEN} Pushed ${CL_END}] "`pwd`
 		return 0
 	elif [ $ret_m == "0" ]; then
-		echo -e "[\e[31m Commit \e[0m] \e[33m"`pwd`"\e[0m"
+		echo -e "[${CL_RED} Commit ${CL_END}] ${CL_YELLOW}"`pwd`"${CL_END}"
 		return 1
 	elif [ $ret == "0" ]; then
-		echo -en "[\e[33m UnPush \e[0m] "`pwd`
-		echo -en "\r[\e[31m Push.. \e[0m] "
+		echo -en "[${CL_YELLOW} UnPush ${CL_END}] "`pwd`
+		echo -en "\r[${CL_RED} Push.. ${CL_END}] "
 		#git push #> /dev/null 2>&1
 		git push > /dev/null 2>&1
 		p_ret=$?
 		if [ ${p_ret} -ne 0 ] ; then
-			echo -e "\n[\e[31m PUSH \e[0m] \e[33m${p_ret}\e[0m" 
+			echo -e "\n[${CL_RED} PUSH ${CL_END}] ${CL_YELLOW}${p_ret}${CL_END}"
 		fi
-		echo -e "\r[\e[33m Pushed \e[0m] "
+		echo -e "\r[${CL_YELLOW} Pushed ${CL_END}] "
 		return 0
 	elif [ $ret_a == "0" ]; then
-		echo -e "[\e[31m Un-Add \e[0m] \e[33m"`pwd`"\e[0m"
+		echo -e "[${CL_RED} Un-Add ${CL_END}] ${CL_YELLOW}"`pwd`${CL_END}
 		return 2
 	fi
 }
 
-#echo de: $aRepos n= $lenRepo .
-#检查所有仓库
-i=0
-while [ $i -lt $lenRepo ]
-do
-	#echo i=$i ${aRepos[$i]}
-	isPushed ${aRepos[$i]}
-	ret=$?
-	if [ $ret == "0" ] ; then
-		let n_push++
-	elif [ $ret == "1" ]; then
-		let n_commit++
-	elif [ $ret == "2" ]; then
-		let n_add++
-	fi
-	let i++
-done
-
-#检查完毕,总结
-echo -en "\e[32m 检查完毕\e[0m: "
-if [ $n_commit == "0" ] && [ $n_add == "0" ]; then
-	echo -en "所有仓库均已push"
-else
-	echo -en "有 \e[31m$n_commit\e[0m 个仓库没有commit"
-fi
-echo ""
+# tty color define
+CL_RED="\033[31m"
+CL_YELLOW="\033[33m"
+CL_GREEN="\033[32m"
+CL_PURP="\033[35m"
+CL_END="\033[0m"
+## start script
 
 main
 
